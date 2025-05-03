fe6cddb70aedaf2e2c4dbded80b83a7656fa2c33
4nder <itzandersonfpcalt@gmail.com>
2025-05-03 13:59:37 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let add_bot_1 = document.getElementById("add_btn");
+let add_bot_2 = document.getElementById("add_btn_2");
+
+add_bot_1.onclick = function() {
+    window.location.href = "#";
+}
+
+add_bot_2.onclick = function() {
+    window.location.href = "#";
+}
